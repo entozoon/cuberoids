5505e60abe01afb14e29abbdb3f538e00d0497c4
Mickey <michael.cook@techdept.co.uk>
2022-01-12 17:58:45 +0000

    Boom, spotlight on ship

--- lib/Camera.ts
+++ lib/Camera.ts
@@ -1,16 +1,7 @@
 import * as THREE from "three";
-const createTorch = () => {
-  //  SpotLight / DirectionalLight
-  const torch = new THREE.PointLight(0xffffff, 1);
-  torch.castShadow = true;
-  // torch.shadow.camera.near = 1; // default .5
-  // torch.shadow.camera.far = 10; // default 500
-  return torch;
-};
 export class Camera {
   camera: THREE.Camera;
   pov: THREE.Object3D;
-  private torch;
   constructor({ pov, scene }: { pov: THREE.Object3D; scene: THREE.Scene }) {
     this.pov = pov;
     this.camera = new THREE.PerspectiveCamera(
@@ -18,13 +9,7 @@ export class Camera {
       window.innerWidth / window.innerHeight
     ); //      ⭡y
     //       z ↙⭢ x
-    this.torch = createTorch();
-    this.camera.add(this.torch);
     scene.add(this.camera);
-    // scene.add(this.torch, this.torch.target);
-    // , this.torch.target);
-    // scene.add(this.torch.target);
-    scene.add(new THREE.CameraHelper(this.torch.shadow.camera));
   }
   update() {
     this.camera.position.set(
@@ -33,12 +18,5 @@ export class Camera {
       this.pov.position.z + 30
     );
     this.camera.rotation.set(-0.2, 0, 0);
-
-    // this.torch.position.copy(this.object.position);
-    // this.torch.target.position.copy(this.object.position);
-    // this.torch.target.position.z -= 1;
-    // // this.torch.updateMatrix();
-    // // this.torch.rotation.set(Math.sin(time / 500) * 2 * Math.PI, 0, 0);
-    // // this.torch.target(0, 0, 0);
   }
 }
--- lib/Ship.ts
+++ lib/Ship.ts
@@ -1,15 +1,38 @@
 import * as THREE from "three";
+const createTorch = () => {
+  //  SpotLight / DirectionalLight / PointLight
+  const torch = new THREE.SpotLight(0xffffff, 1);
+  torch.castShadow = true;
+  torch.shadow.camera.near = 100; // default .5 (flickering)
+  // torch.shadow.camera.far = 10; // default 500
+  return torch;
+};
 export class Ship {
   public object: THREE.Object3D;
+  private torch;
   constructor(scene: THREE.Scene) {
     const geometry = new THREE.BoxGeometry(6, 1, 2);
     const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
     this.object = new THREE.Mesh(geometry, material);
     scene.add(this.object);
+    this.torch = createTorch();
+    this.object.add(this.torch);
+    scene.add(this.torch, this.torch.target);
+    // scene.add(new THREE.CameraHelper(this.torch.shadow.camera));
   }
   update(time) {
     this.object.position.x = Math.sin(time / 1000) * 20;
     this.object.position.y = Math.cos(time / 1000) * 100;
     this.object.position.z = Math.cos(time / 1000) * 20;
+    this.torch.position.set(
+      this.object.position.x,
+      this.object.position.y,
+      this.object.position.z - 5
+    );
+    this.torch.target.position.set(
+      this.object.position.x,
+      this.object.position.y,
+      this.object.position.z - 100
+    );
   }
 }
