e5319c5a1265af30a03c0b5e4059b43d00d90dc6
Mickey <michael.cook@techdept.co.uk>
2022-01-12 15:21:47 +0000

    Gonna have to figure out ship flying mech soon..

--- lib/game.ts
+++ lib/game.ts
@@ -7,7 +7,7 @@ export default class Game {
   rendererOrEffect: THREE.WebGLRenderer;
   effect;
   box: THREE.BoxGeometry;
-  planeFill: THREE.Mesh;
+  planeMesh: THREE.Mesh;
   planeGrid: THREE.Mesh;
   light1;
   lightbox1;
@@ -19,54 +19,46 @@ export default class Game {
       window.innerWidth / window.innerHeight,
       0.01,
       1000
-    ); // x,y,z (y is up)
-    this.camera.position.z = 100;
+    ); //      ⭡y
+    //       z ↙⭢ x
+    this.camera.position.set(0, -900, 0);
+    this.camera.rotation.set(Math.PI / 2, 0, 0);
     this.scene = new THREE.Scene();
-    // const box = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    // const material = new THREE.MeshNormalMaterial();
-    // this.mesh = new THREE.Mesh(box, material);
-    // this.scene.add(this.mesh);
-    const geometry = new THREE.PlaneGeometry(100, 100, 100, 100); // 1 unit = 1 meter,
-    for (let i = 0; i < geometry.attributes.position.array.length; i += 3) {
+    const planeGeometry = new THREE.PlaneGeometry(100, 100, 100, 100); // 1 unit = 1 meter,
+    for (
+      let i = 0;
+      i < planeGeometry.attributes.position.array.length;
+      i += 3
+    ) {
       // @ts-ignore
-      geometry.attributes.position.array[i + 2] =
+      planeGeometry.attributes.position.array[i + 2] =
         Math.sin(i / 1000) * 2 + Math.random() * 2; // x, y, z++
     }
     //
     // FILL (these things can be grouped together. Maybe whole planet)
     const materialFill = new THREE.MeshPhongMaterial({
-      color: 0x666666,
+      color: 0x66ff66,
       side: THREE.DoubleSide, // debug only
       // shininess: 50,
     });
-    this.planeFill = new THREE.Mesh(geometry, materialFill);
-    this.planeFill.castShadow = true;
-    this.planeFill.receiveShadow = true;
-    this.scene.add(this.planeFill);
+    this.planeMesh = new THREE.Mesh(planeGeometry, materialFill);
+    this.planeMesh.castShadow = true;
+    this.planeMesh.receiveShadow = true;
+    this.planeMesh.position.set(0, 0, 0);
+    this.scene.add(this.planeMesh);
     //
-    // GRID
-    // const materialGrid = new THREE.MeshPhongMaterial({
-    //   color: 0xffffff,
-    //   opacity: 0.1,
-    //   transparent: true,
-    //   side: THREE.DoubleSide, // debug only
-    //   wireframe: true,
-    // });
-    // this.planeGrid = new THREE.Mesh(geometry, materialGrid);
-    // this.planeGrid.castShadow = true;
-    // this.planeGrid.receiveShadow = true;
-    // this.scene.add(this.planeGrid);
-    const lightAmbient = new THREE.AmbientLight(0xaa2222);
+    const lightAmbient = new THREE.AmbientLight(0xffffff, 0.05);
     lightAmbient.castShadow = true;
-    // // this.scene.add(lightAmbient);
-    this.light2 = new THREE.SpotLight(0xff6666, 1);
+    this.scene.add(lightAmbient);
+    //
+    this.light2 = new THREE.SpotLight(0xffffff, 1);
     this.light2.castShadow = true;
     // this.light2.shadow.camera.near = 1; // default .5
     this.light2.shadow.camera.far = 10; // default 500
     this.light2.position.set(5, 5, 0);
     this.scene.add(this.light2);
     const lightbox2Geometry = new THREE.BoxGeometry(1, 1, 1);
-    const lightbox2Material = new THREE.MeshBasicMaterial({ color: 0xff6666 });
+    const lightbox2Material = new THREE.MeshBasicMaterial({ color: 0xffffff });
     this.lightbox2 = new THREE.Mesh(lightbox2Geometry, lightbox2Material);
     this.lightbox2.position.set(5, 5, 0);
     this.scene.add(this.lightbox2);
@@ -85,7 +77,7 @@ export default class Game {
       : renderer;
   }
   animation(time) {
-    // this.planeFill.rotation.y = time / 1000;
+    // this.planeMesh.rotation.y = time / 2000;
     this.lightbox2?.position.set(
       Math.cos(-time / 1000) * 70,
       Math.sin(time / 1000) * 70,
