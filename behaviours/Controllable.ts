be5af9a72148f99cb7aeafa9c942e2542673f5e1
Mickey <michael.cook@techdept.co.uk>
2022-01-13 20:21:45 +0000

    Just enough control to move to camera issues

--- behaviours/Controllable.ts
+++ behaviours/Controllable.ts
@@ -1,3 +1,5 @@
+import { Vector3 } from "three";
+
 export default class {
   private parent;
   private reservedKeys = ["w", "a", "s", "d", "q", "e", " "];
@@ -12,26 +14,21 @@ export default class {
       e.stopPropagation();
       e.preventDefault();
     }
-    let rotationNew = {
-      x: rotation.x,
-      y: rotation.y,
-      z: rotation.z,
-    };
-    console.log(rotationNew);
+    let _rotation = rotation.toVector3();
     if (key === " ") {
       // Incorrect for orientation, but yeah. normals?
       position.set(0, 0, position.z - 1);
     }
+    // Will scrap when figuring out impulse
     if (key === "w") {
-      rotationNew.x += 0.1;
+      _rotation.add(new Vector3(0.1, 0, 0));
     }
     if (key === "e") {
-      // rotation.set(rotation.x, rotation.y - 0.1, rotation.z);
+      _rotation.add(new Vector3(0, 0.1, 0));
     }
     if (key === "d") {
-      // rotation.set(rotation.x, rotation.y.z + 0.1);
+      _rotation.add(new Vector3(0, 0, 0.1));
     }
-    // Vector3 (0,0,0) style. not quite sure how to interact with them individually
-    rotation.set(...Object.values(rotationNew));
+    rotation.setFromVector3(_rotation);
   }
 }
